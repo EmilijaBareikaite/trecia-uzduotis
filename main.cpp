5d0f4d42692b147e2182c734a07ba894a42acaa2
Emilija <145277411+EmilijaBareikaite@users.noreply.github.com>
2023-09-27 20:56:32 +0300

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,35 +1,37 @@
 
 #include "my_lib.h"
 
-
-
 int main() {
-    
     
     
     studentas laikinas;
     vector <studentas> grupe;
-    int m;
+    int m, k;
     cout<<"Kiek studentų mokosi? ";
     cin>>m;
-    cout<<"Ivesk kiek pazymiu turi studentai: ";
-    int n;
-    cin>>n;
-    //mediana
+   
     
-    
+ 
     for (int j=0; j<m; j++) {
-        cout<<"Iveskite studento varda ir pavarde: ";
+        cout<<"Įveskite studento vardą ir pavardę: ";
         cin>>laikinas.vard>>laikinas.pavard;
-        for (int i=0; i<n; i++) {
-            int k;
-            cout<<"Iveskite "<<i+1<<" pazymi: ";
-            cin>>k;
+    
+        cout<<"Įveskite studento pažymį (parašę '-2' baigsite vesti studento pažymius): ";
+        cin>>k;
+        
+        int n=0;
+        while ( k != -2)
+        {
+            n++;
             laikinas.paz.push_back(k);
+            cout<<"Įveskite studento pažymį (parašę '-2' baigsite vesti studento pažymius):  ";
+            cin>>k;
+           
         }
+        
         sort(laikinas.paz.begin(), laikinas.paz.end());
         
-        cout<<"iveskite egzamino rez: ";
+        cout<<"Įveskite egzamino rezultatą: ";
         cin>>laikinas.egz;
         
         //skaiciuojam vidurki
@@ -44,9 +46,9 @@ int main() {
         
         //skaiciuojam mediana
         if (n % 2 == 0)
-            laikinas.mediana = (laikinas.paz[n/2]+laikinas.paz[(n/2)-1])/2.0;
+            laikinas.mediana = (laikinas.paz.at(n/2)+laikinas.paz.at((n/2)-1))/2.0;
         else
-            laikinas.mediana = laikinas.paz[(n/2)];
+            laikinas.mediana = laikinas.paz.at(n/2);
     
         grupe.push_back(laikinas);
         laikinas.paz.clear(); //grupeje laikomi pazymiai
@@ -58,18 +60,18 @@ int main() {
     cout<<"Ar norite, kad rodytų studentų galutinį vidurkį ar medianą? Rašykite 'v', jeigu norite vidurkio, ir 'm', jeigu norite medianos. ";
     cin>>spausd;
     if (spausd == 'v') {
-        printf("%-20s%-20s%-10s\n", "Pavardė","Vardas","Galutinis (Vid.)" );
-        cout<<"-----------------------------------"<<endl;
+        printf("%-25s%-25s%-15s\n", "Pavardė","Vardas","Galutinis (Vid.)" );
+        cout<<"-----------------------------------------------------------"<<endl;
         for (auto &a: grupe){
-            cout<<left<<setw(20)<<a.pavard<<setw(20)<<a.vard<<setw(10)<<fixed<<setprecision(2)<<a.rez<<endl;
+            cout<<left<<setw(25)<<a.pavard<<setw(25)<<a.vard<<setw(15)<<fixed<<setprecision(2)<<a.rez<<endl;
         }
     }
     else
     {
-        printf("%-20s%-20s%-10s\n", "Pavardė","Vardas","Galutinis (Med.)" );
-        cout<<"-----------------------------------"<<endl;
+        printf("%-25s%-25s%-15s\n", "Pavardė","Vardas","Galutinis (Med.)" );
+        cout<<"----------------------------------------------------"<<endl;
         for (auto &a: grupe){
-            cout<<left<<setw(20)<<a.pavard<<setw(20)<<a.vard<<setw(10)<<fixed<<setprecision(2)<<a.mediana<<endl;
+            cout<<left<<setw(25)<<a.pavard<<setw(25)<<a.vard<<setw(15)<<fixed<<setprecision(2)<<a.mediana<<endl;
             
         }
     }
